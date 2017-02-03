67d220f9ec1aeace4111532a99b941adb7c4f0ee
Daniel Melchior <daniel@whispeer.de>
2017-02-03 11:41:55 +0100

    login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,14 @@ import { Component } from "@angular/core";
 import { Platform } from "ionic-angular";
 import { StatusBar, Splashscreen } from "ionic-native";
 
-import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
 
 
 @Component({
 	templateUrl: "app.html"
 })
 export class MyApp {
-	rootPage = HomePage;
+	rootPage = LoginPage;
 
 	constructor(platform: Platform) {
 		platform.ready().then(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+
+import { LoginPage } from "../pages/login/login"
+import { HomePage } from "../pages/home/home";
 import { MessagesPage } from "../pages/messages/messages";
 
 import { NewMessageModal } from "../pages/home/newMessage.modal";
@@ -13,10 +13,9 @@ import { UserService } from "../assets/services/user.service";
 @NgModule({
 	declarations: [
 		MyApp,
-		AboutPage,
-		ContactPage,
 		HomePage,
 		MessagesPage,
+		LoginPage,
 		NewMessageModal
 	],
 	imports: [
@@ -25,12 +24,14 @@ import { UserService } from "../assets/services/user.service";
 	bootstrap: [IonicApp],
 	entryComponents: [
 		MyApp,
-		AboutPage,
-		ContactPage,
 		HomePage,
 		MessagesPage,
+		LoginPage,
 		NewMessageModal
 	],
-	providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, UserService]
+	providers: [
+		{provide: ErrorHandler, useClass: IonicErrorHandler},
+		UserService
+	]
 })
 export class AppModule {}
