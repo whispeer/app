1e025c4569052b9e568a5eff8b467ad3244e18a3
Daniel Melchior <daniel@whispeer.de>
2017-02-03 17:15:33 +0100

    message list and input

--- src/pages/messages/messages.ts
+++ src/pages/messages/messages.ts
@@ -1,29 +1,59 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 
-import { NavController} from "ionic-angular";
+import { NavController, Content, Platform } from "ionic-angular";
+
+import { Keyboard } from "ionic-native";
+import { Subscription } from 'rxjs/rx';
 
 @Component({
 	selector: 'page-messages',
 	templateUrl: 'messages.html'
 })
 export class MessagesPage {
+	@ViewChild(Content) content: Content;
+
+	private onShowSubscription: Subscription;
+
+	i: number = 20;
 	messages: any[];
-	constructor(public navCtrl: NavController) {}
+	constructor(public navCtrl: NavController, private platform: Platform) {
+		if (this.platform.is('cordova') && this.platform.is('ios')) {
+			this.onShowSubscription = Keyboard.onKeyboardShow().subscribe((e) => {
+				debugger;
+			});
+		}
+	}
 
 	ngOnInit() {
 		this.messages = [];
-		for(let i = 0; i < 10000; i++) {
+		for(let i = 0; i < 10; i++) {
 			const b = i % 2;
 			this.messages.push({
 				id: i,
-				user: {
-					isMe: b,
-					name: b ? "Daniel" : "Steffen"
+				isMe: () => {
+					return b;
 				},
-				text: "Hi! I am the default message.",
+				isOther: () => {
+					return !b;
+				},
+				sender: () => {
+
+				},
+				messages: [{
+					text: "Hi! I am the default message."
+				}, {
+					text: "I am the latest one."
+				}],
 				date: new Date()
 			});
 		}
 	}
 
+	ionViewDidEnter = () => {
+		this.content.scrollToBottom(0);
+	}
+
+	messageBursts = () => {
+		return this.messages;
+	}
 }
