250ecefbac3e4f3f07b47112c0d53edff1a88249
Nils Kenneweg <nk@ovt.me>
2017-02-07 21:20:08 +0100

    working profile page except request accepting

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import sessionService from '../../assets/services/session.service';
+import * as Bluebird from 'bluebird';
 
-var userService = require("user/userService");
+const userService = require("user/userService");
+const friendsService = require("../../assets/services/friendsService");
 
 /*
 	Generated class for the Profile page.
@@ -16,7 +18,8 @@ var userService = require("user/userService");
 })
 export class ProfilePage {
 	user: any = {
-		basic: {}
+		basic: {},
+		names: {}
 	};
 
 	userObject: any;
@@ -34,30 +37,33 @@ export class ProfilePage {
 
 	constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
-	ionViewDidLoad() {
-		console.log('ionViewDidLoad ProfilePage');
-	}
-
 	ngOnInit() {
 		this.userId = parseFloat(this.navParams.get("userId"));
 
 		this.isOwn = this.userId === sessionService.userid;
-		this.isRequest = false;
 
-		userService.get(this.userId).then((user) => {
+		const awaitFriendsService = friendsService.awaitLoading().then(() => {
+			var requests = friendsService.getRequests();
+
+			this.isRequest = requests.indexOf(this.userId) > -1
+		});
+
+		Bluebird.all([
+			userService.get(this.userId),
+			awaitFriendsService
+		]).then(([user]) => {
 			if (user.isNotExistingUser()) {
 				this.user = user.data;
 				this.profileLoading = false;
 				return;
 			}
 
-			this.userObject = user;
-
 			var fp = user.getFingerPrint();
 			this.fingerprint = [fp.substr(0,13), fp.substr(13,13), fp.substr(26,13), fp.substr(39,13)];
 
-			return user.loadFullData();
-		}).then(() => {
+			return user.loadFullData().thenReturn(user);
+		}).then((user) => {
+			this.userObject = user;
 			this.user = this.userObject.data;
 
 			this.profileLoading = false;
