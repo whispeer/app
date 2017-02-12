e5f6a40517858a5997ce6e9099b16a4bc0d90d07
Martin Czuchra <martin.czuchra@googlemail.com>
2017-02-12 16:18:59 +0100

    Allows registrations.

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -8,12 +8,10 @@ import jQuery from "jquery";
 const registerService = require('../../assets/services/registerService');
 const whispeerHelper = require('whispeerHelper')
 
-
 @Component({
 	selector: 'page-login',
 	templateUrl: 'login.html'
 })
-
 
 export class LoginPage {
 
@@ -24,7 +22,6 @@ export class LoginPage {
 
 	constructor(public navCtrl: NavController, public navParams: NavParams) {
 		this.login = loginService;
-
 		loginService.loadedStorage.then(() => {
 			this.checkUserNameExistance();
 			(loginService.identifier
@@ -43,8 +40,14 @@ export class LoginPage {
 				return INVALID_USERNAME;
 			case USERNAME_LOGIN_SUCCESS:
 				return LOGIN_SUCCESS;
+			case USERNAME_REGISTER_SUCCESS:
+				return REGISTER_SUCCESS;
 			case USERNAME_LOGIN_ERROR:
+			case USERNAME_REGISTER_ERROR:
 				return GENERIC_ERROR_MESSAGE;
+			case USERNAME_PASSWORD_CONFIRM:
+				return CONFIRM_PASSWORD;
+
 			case USERNAME_EMPTY:
 			case USERNAME_UNKNOWN:
 			default:
@@ -54,6 +57,14 @@ export class LoginPage {
 
 	ionViewDidLoad() {
 		console.log("ionViewDidLoad LoginPage");
+	}
+
+	showPasswordFeld() {
+		return !this.showPasswordConfirmationField()
+	}
+
+	showPasswordConfirmationField() {
+		return [USERNAME_PASSWORD_CONFIRM].indexOf(this.usernameState) !== -1
 	}
 
 	checkUserNameExistance = () => {
@@ -71,26 +82,91 @@ export class LoginPage {
 		}
 	}
 
+	performLogin() {
+		const { identifier: nick, password: pass } = this.login;
+		return loginService.loginServer(nick, pass);
+	}
+
+	performRegister() {
+		const { identifier: nick, password: pass } = this.login;
+		const email = '';
+		const defaultSettings = {
+			meta: {
+				initialLanguage: "en"
+			},
+			content: {}
+		};
+		const defaultProfile = {
+			pub: {},
+			priv: {},
+			nobody: {},
+			metaData: {
+				scope: "always:allfriends"
+			}
+		};
+		return registerService.register(nick, email, pass, defaultProfile, defaultSettings);
+	}
+
+	passwordsMatch() {
+		return (
+			(this.login.password === this.passwordRepeat)
+			&& (this.login.password.length !== 0)
+		)
+	}
+
+	reset() {
+		this.login.identifier = '';
+		this.login.password = '';
+		this.passwordRepeat = '';
+		this.checkUserNameExistance();
+	}
+
 	loginOrRegister = () => {
-		const { identifier: nick, password: pass } = this.login
 		if ([USERNAME_TAKEN, USERNAME_LOGIN_ERROR].indexOf(this.usernameState) !== -1) {
-			loginService.loginServer(nick, pass).then(() => {
+
+			// login
+			this.performLogin().then(() => {
 				this.usernameState = USERNAME_LOGIN_SUCCESS
 				mainPage()
 			}).catch((error) => {
-				console.log('error')
 				this.usernameState = USERNAME_LOGIN_ERROR
 			})
+
+		} else if ([USERNAME_FREE].indexOf(this.usernameState) !== -1) {
+
+			this.usernameState = USERNAME_PASSWORD_CONFIRM
+
+		} else if ([USERNAME_PASSWORD_CONFIRM].indexOf(this.usernameState) !== -1) {
+			const registerError = () => { this.usernameState = USERNAME_REGISTER_ERROR; }
+
+			if (this.passwordsMatch()) {
+				this.performRegister().then(() => {
+					this.usernameState = USERNAME_REGISTER_SUCCESS;
+
+					this.performLogin().then(() => {
+						mainPage()
+					}).catch(registerError)
+
+				}).catch(registerError);
+
+			} else { registerError() }
+
+		} else if ([USERNAME_REGISTER_ERROR].indexOf(this.usernameState) !== -1) {
+
+			this.reset();
+
 		}
 	}
 }
 
-const INVALID_USERNAME = "Gna!"
+const INVALID_USERNAME = "Please pick another name!"
 const DEFAULT_CALL_TO_ACTION = "Create a new account or simply login..."
 const AMBIGUOUS_PROCESS = "This username exists. You can choose to continue login or register a distinct username"
 const LOGIN_SUCCESS = "Success! We'll log you in securely now..."
 const REGISTER_MESSAGE = "This username is free. You can continue to choose a secure password..."
 const GENERIC_ERROR_MESSAGE = "Something went wrong."
+const CONFIRM_PASSWORD = "Great! Now verify your new password"
+const REGISTER_SUCCESS = "Welcome on board! We'll log you in now..."
 
 const USERNAME_UNKNOWN = -1;
 const USERNAME_EMPTY = 1;
@@ -99,3 +175,6 @@ const USERNAME_TAKEN = 3;
 const USERNAME_FREE = 4;
 const USERNAME_LOGIN_ERROR = 5;
 const USERNAME_LOGIN_SUCCESS = 6;
+const USERNAME_PASSWORD_CONFIRM = 7;
+const USERNAME_REGISTER_SUCCESS = 8;
+const USERNAME_REGISTER_ERROR = 9;
