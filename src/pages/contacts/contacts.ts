9c1344ceaba8eab80b5296981f2ef9b9b59717b4
Nils Kenneweg <nk@ovt.me>
2017-02-20 13:16:16 +0100

    add a search to user contacts page

--- src/pages/contacts/contacts.ts
+++ src/pages/contacts/contacts.ts
@@ -8,8 +8,11 @@ import { HomePage } from "../home/home";
 
 const friendsService = require("../../assets/services/friendsService");
 const userService = require("../../assets/user/userService");
+import errorService from "../../assets/services/error.service";
 
 import * as Bluebird from 'bluebird';
+
+const h = require("whispeerHelper");
 
 @Component({
 	selector: 'page-contacts',
@@ -18,7 +21,12 @@ import * as Bluebird from 'bluebird';
 export class ContactsPage {
 	friends: any[] = [];
 	friendsLoading: boolean = true;
+
 	requests: any[] = [];
+	searchResults: any[] = [];
+	searchResultsLoading: boolean = false;
+
+	searchTerm: string = "";
 
 	constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
@@ -27,7 +35,7 @@ export class ContactsPage {
 
 		return Bluebird.try(() => {
 			var friends = friendsService.getFriends();
-			return userService.getMultipleFormatted(friends);
+			return userService.getMultipleFormatted(friends.slice(0, 5));
 		}).then((result: any[]) => {
 			return result.sort((a: any, b: any): number => {
 				const firstAvailable = a.names.firstname && b.names.firstname;
@@ -59,6 +67,10 @@ export class ContactsPage {
 	friendDividers = (record, recordIndex, records) => {
 		const firstChar: string = record.name[0];
 
+		if (this.searchTerm) {
+			return null;
+		}
+
 		if(recordIndex === 0) {
 			return firstChar.toUpperCase();
 		}
@@ -68,6 +80,51 @@ export class ContactsPage {
 		}
 
 		return null;
+	}
+
+	executeSearch = h.debounce(() => {
+		console.log(`searching for ${this.searchTerm}`);
+
+		if (this.searchTerm.length < 3) {
+			this.searchResults = [];
+		}
+
+		this.searchResultsLoading = true;
+
+		const query = this.searchTerm;
+		const friends = friendsService.getFriends();
+
+		userService.query(query).bind(this).filter((user) => {
+			return friends.indexOf(user.getID()) === -1;
+		}).map(function (user) {
+			if (this.searchTerm !== query) {
+				return;
+			}
+
+			return user.loadBasicData().thenReturn(user);
+		}).then(function (users) {
+			if (this.searchTerm !== query) {
+				return;
+			}
+
+			return users.map(function (user) {
+				user.loadFullData(errorService.criticalError);
+				return user.data;
+			});
+		}).then((userData) => {
+			this.searchResults = userData;
+			this.searchResultsLoading = false;
+		});
+	}, 100)
+
+	getFriends = () =>  {
+		if (!this.searchTerm) {
+			return this.friends;
+		}
+
+		return this.friends.filter((friend) => {
+			return friend.name.indexOf(this.searchTerm) > -1;
+		});
 	}
 
 	goToUser(userId) {
