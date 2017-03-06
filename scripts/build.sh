1cb898f6b8ecbf231828465fe26ff028200e681b
Nils Kenneweg <nk@ovt.me>
2017-03-06 18:22:00 +0100

    add build with crosswalk

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,46 @@
-set -x
+#!/bin/bash
 set -e
+set -x
+
+function signAndCopy {
+	cp ./platforms/android/build/outputs/apk/$name-unsigned.apk ./release
+
+	jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ./release/my-release-key.keystore ./release/$name-unsigned.apk alias_name
+
+	rm -f ./release/whispeer-$extra.apk
+
+	zipalign -v 4 ./release/$name-unsigned.apk ./release/whispeer-$extra.apk
+
+	rm -f ./release/$name-unsigned.apk
+}
+
+echo "build for android >= 5"
+
+name=android-release
+extra=android
+
+sed -i .bak 's/android-minSdkVersion" value="16"/android-minSdkVersion" value="21"/g' config.xml
+rm -rf platforms
+rm -rf plugins
+cordova prepare
+
+# sed -i .bak 's/^ext.cdvMinSdkVersion/\/\/ext.cdvMinSdkVersion/g' ./plugins/phonegap-plugin-barcodescanner/src/android/barcodescanner.gradle
 
 ionic build android --prod --release
+mv config.xml.bak config.xml
 
-cp platforms/android/build/outputs/apk/android-release-unsigned.apk release
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ./release/my-release-key.keystore ./release/android-release-unsigned.apk alias_name
-rm -f release/whispeer.apk
-zipalign -v 4 release/android-release-unsigned.apk release/whispeer.apk
+signAndCopy
+
+echo "build for android 4"
+
+name=android-armv7-release
+extra=android4
+
+cordova plugin add cordova-plugin-crosswalk-webview@1.6.1
+
+ionic build --release android
+signAndCopy
+
+rm -rf platforms
+rm -rf plugins
+cordova prepare
