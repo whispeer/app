6ffbb5c4131a4e47c3d651ec06d119f74dcfa996
Nils Kenneweg <nk@ovt.me>
2017-03-20 18:04:15 +0100

    import toBlob polyfill in main

--- src/app/main.ts
+++ src/app/main.ts
@@ -4,6 +4,8 @@ import "rxjs/add/operator/catch";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/toPromise";
 
+import "toBlob";
+
 import { AppModule } from "./app.module";
 
 platformBrowserDynamic().bootstrapModule(AppModule);
