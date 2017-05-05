ddb01f25e1ec912845bf74ec944e27d9154b933e
Nils Kenneweg <nk@ovt.me>
2017-05-05 09:34:50 +0200

    new task runner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -53,16 +53,57 @@ import { BarcodeScanner } from "@ionic-native/barcode-scanner";
 	]
 })
 export class AppModule {
+	tasks: any[] = []
+	taskRunnerStarted: Boolean = false
+
+	startTaskRunner = () => {
+		if (this.taskRunnerStarted || this.tasks.length === 0) {
+			return
+		}
+
+		this.taskRunnerStarted = true
+		setTimeout(this.runTasks, 0)
+	}
+
+	runTasks = () => {
+		this.taskRunnerStarted = false
+
+		const tasksStarted = Date.now()
+		const maxTaskCount = this.tasks.length
+
+		for (let i = 0; i < maxTaskCount; i += 1) {
+			const task = this.tasks.shift()
+			task()
+
+			const dateDiff = Date.now() - tasksStarted
+
+			if (dateDiff > 50) {
+				console.error(`Long running task detected ${dateDiff}`, task)
+			}
+
+			if (i + 1 !== maxTaskCount && dateDiff > 20) {
+				// console.warn(`Breaking out of tasks loop ${i+1} / ${maxTaskCount} / ${this.tasks.length}: ${dateDiff}`)
+				break;
+			}
+		}
+
+		this.startTaskRunner()
+	}
+
+	runInAngularZone(fn) {
+		if((<any>this.zone).inner !== (<any>window).Zone.current) {
+			this.zone.run(fn)
+			return
+		}
+
+		fn()
+	}
+
 	constructor(private zone: NgZone) {
 		Bluebird.setScheduler((fn) => {
-			setTimeout(() => {
-				if((<any>this.zone).inner !== (<any>window).Zone.current) {
-					this.zone.run(fn)
-					return
-				}
+			this.tasks.push(fn)
 
-				fn()
-			}, 0);
+			this.runInAngularZone(this.startTaskRunner)
 		});
 	}
 }
