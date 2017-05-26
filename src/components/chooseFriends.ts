f8a917b22ed0fc7dadc79fc98f0c064b431ee18c
Nils Kenneweg <nk@ovt.me>
2017-05-26 17:58:18 +0200

    working selects. not a11y but otherwise fine

--- src/components/chooseFriends.ts
+++ src/components/chooseFriends.ts
@@ -1,5 +1,5 @@
 import { Component, Output, EventEmitter } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Platform, NavController } from 'ionic-angular';
 
 const friendsService = require("../lib/services/friendsService");
 
@@ -17,11 +17,14 @@ export class ChooseFriends extends ContactsWithSearch {
 	searchTerm: string = "";
 	selectedUserMap: any = {};
 	selectedUsers: any[] = [];
+	ios: boolean = false;
 
 	@Output() chooseReceivers = new EventEmitter();
 
-	constructor(public navCtrl: NavController, private translate: TranslateService) {
+	constructor(public navCtrl: NavController, private translate: TranslateService, private platform: Platform) {
 		super()
+
+		this.ios = platform.is("ios")
 	}
 
 	ngOnInit() {
@@ -35,17 +38,21 @@ export class ChooseFriends extends ContactsWithSearch {
 		if (this.selectedUsers.indexOf(user) === -1) {
 			this.selectedUsers.push(user);
 		}
+
+		this.selectedUserMap[user.id] = true
 	}
 
 	removeSelectedUser = (user) => {
 		h.removeArray(this.selectedUsers, user);
+
+		delete this.selectedUserMap[user.id]
 	}
 
 	updateSelectedUsers = (user) => {
 		if (this.selectedUserMap[user.id]) {
-			this.addSelectedUser(user);
-		} else {
 			this.removeSelectedUser(user);
+		} else {
+			this.addSelectedUser(user);
 		}
 	}
 
