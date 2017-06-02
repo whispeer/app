1d8ed126d9313e06ab34e7c5d1360dc669d0bab3
Martin Czuchra <martin.czuchra@googlemail.com>
2017-06-02 17:35:27 +0200

    Add a details page under #/details.

---
+++ src/pages/messages/details/details.ts
@@ -0,0 +1,16 @@
+import { Component, ElementRef } from "@angular/core";
+import { NavParams, IonicPage } from "ionic-angular";
+
+import TopicUpdate from "../../lib/messages/topicTitleUpdate";
+import errorService from "../../lib/services/error.service";
+
+@IonicPage({
+	name: "Chat Details",
+	segment: "details"
+})
+@Component({
+	selector: 'page-details',
+	templateUrl: 'details.html'
+})
+export class DetailPage {
+}
