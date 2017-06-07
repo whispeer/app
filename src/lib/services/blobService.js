d19613ba807a29a0462ceb3d609d2129fc264111
Nils Kenneweg <nk@ovt.me>
2017-06-07 12:01:01 +0200

    polyfills need toload before cordova plugins

--- src/lib/services/blobService.js
+++ src/lib/services/blobService.js
@@ -85,10 +85,6 @@ MyBlob.prototype.getArrayBuffer = function () {
 	return new Bluebird(function (resolve) {
 		var reader = new FileReader();
 
-		if (reader._realReader) {
-			reader = reader._realReader
-		}
-
 		if (reader.addEventListener) {
 			reader.addEventListener("loadend", resolve);
 		} else {
