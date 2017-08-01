77c44f8a8e54b781d6325ab972b05c89717eb8b7
Nils Kenneweg <nk@ovt.me>
2017-08-01 10:03:20 +0200

    refactor blob service to es6

--- src/lib/services/blobService.js
+++ src/lib/services/blobService.js
@@ -22,275 +22,268 @@ downloadBlobQueue.start();
 var debugName = "whispeer:blobService";
 var blobServiceDebug = debug(debugName);
 
-function time(name) {
+const time = (name) => {
 	if (debug.enabled(debugName)) {
+		// eslint-disable-next-line no-console
 		console.time(name);
 	}
 }
 
-function timeEnd(name) {
+const timeEnd = (name) => {
 	if (debug.enabled(debugName)) {
+		// eslint-disable-next-line no-console
 		console.timeEnd(name);
 	}
 }
 
 var blobCache = new Cache("blobs");
 
-var MyBlob = function (blobData, blobID, options) {
-	this._blobData = blobData;
-	options = options || {};
+class MyBlob {
+	constructor(blobData, blobID, options) {
+		this._blobData = blobData;
+		options = options || {};
 
-	if (typeof blobData === "string") {
-		this._legacy = true;
-	} else if (blobData instanceof File) {
-		this._file = true;
-	}
+		if (typeof blobData === "string") {
+			this._legacy = true;
+		} else if (blobData instanceof File) {
+			this._file = true;
+		}
 
-	if (blobID) {
-		this._blobID = blobID;
-		this._uploaded = true;
-	} else {
-		this._uploaded = false;
-	}
-
-	this._meta = options.meta || {};
-	this._key = this._meta._key;
-	this._decrypted = !this._key;
-
-	this._uploadProgress = new Progress({ total: this.getSize() });
-	this._encryptProgress = new Progress({ total: this.getSize() });
-	this._decryptProgress = new Progress({ total: this.getSize() });
-};
-
-MyBlob.prototype.isUploaded = function () {
-	return this._uploaded;
-};
-
-MyBlob.prototype.setMeta = function (meta) {
-	if (!this.isUploaded()) {
-		this._meta = meta;
-	}
-};
-
-MyBlob.prototype.getSize = function () {
-	return this._blobData.size;
-};
-
-MyBlob.prototype.getMeta = function () {
-	return this._meta;
-};
-
-MyBlob.prototype.getArrayBuffer = function () {
-	var that = this;
-
-	return new Bluebird(function (resolve) {
-		var reader = new FileReader();
-
-		if (reader.addEventListener) {
-			reader.addEventListener("loadend", resolve);
+		if (blobID) {
+			this._blobID = blobID;
+			this._uploaded = true;
 		} else {
-			reader.onloadend = resolve;
+			this._uploaded = false;
 		}
 
-		reader.readAsArrayBuffer(that._blobData);
-	}).then(function (event) {
-		var target = event.currentTarget || event.target;
+		this._meta = options.meta || {};
+		this._key = this._meta._key;
+		this._decrypted = !this._key;
 
-		return target.result;
-	});
-};
-
-MyBlob.prototype.encryptAndUpload = function (key, cb) {
-	var that = this, blobKey;
-
-	return Bluebird.try(function () {
-		return that.encrypt();
-	}).then(function (_blobKey) {
-		blobKey = _blobKey;
-		return keyStore.sym.symEncryptKey(blobKey, key);
-	}).then(function () {
-		return that.upload();
-	}).then(function () {
-		return blobKey;
-	}).nodeify(cb);
-};
-
-MyBlob.prototype.encrypt = function (cb) {
-	var that = this;
-
-	return Bluebird.resolve().bind(this).then(function () {
-		if (that._uploaded || !that._decrypted) {
-			throw new Error("trying to encrypt an already encrypted or public blob. add a key decryptor if you want to give users access");
-		}
-
-		return Bluebird.all([
-			keyStore.sym.generateKey(null, "blob key"),
-			that.getArrayBuffer()
-		]);
-	}).spread(function (_key, buf) {
-		that._key = _key;
-
-		time("blobencrypt" + (that._blobID || that._preReserved));
-		return keyStore.sym.encryptArrayBuffer(buf, that._key, function (progress) {
-			that._encryptProgress.progress(that.getSize() * progress);
-		});
-	}).then(function (encryptedData) {
-		that._encryptProgress.progress(that.getSize());
-		timeEnd("blobencrypt" + (that._blobID || that._preReserved));
-		blobServiceDebug(encryptedData.byteLength);
-		that._decrypted = false;
-
-		that._blobData = new Blob([encryptedData], {type: that._blobData.type});
-
-		return that._key;
-	}).nodeify(cb);
-};
-
-MyBlob.prototype.decrypt = function (cb) {
-	if (this._decrypted) {
-		return Bluebird.resolve().nodeify(cb);
+		this._uploadProgress = new Progress({ total: this.getSize() });
+		this._encryptProgress = new Progress({ total: this.getSize() });
+		this._decryptProgress = new Progress({ total: this.getSize() });
 	}
 
-	return Bluebird.try(() => {
-		return this.getArrayBuffer();
-	}).then((encryptedData) => {
-		time("blobdecrypt" + this._blobID);
-		return keyStore.sym.decryptArrayBuffer(encryptedData, this._key, (progress) => {
-			this._decryptProgress.progress(this.getSize() * progress)
-		})
-	}).then((decryptedData) => {
-		this._decryptProgress.progress(this.getSize())
-		timeEnd("blobdecrypt" + this._blobID);
+	isUploaded() {
+		return this._uploaded;
+	}
 
-		this._decrypted = true;
+	setMeta (meta) {
+		if (!this.isUploaded()) {
+			this._meta = meta;
+		}
+	}
 
-		this._blobData = new Blob([decryptedData], {type: this._blobData.type});
-	}).nodeify(cb);
-};
+	getSize() {
+		return this._blobData.size;
+	}
 
-MyBlob.prototype.getBase64Representation = function () {
-	return this.getStringRepresentation().then(function (blobValue) {
-		return blobValue.split(",")[1];
-	});
-};
+	getMeta() {
+		return this._meta;
+	}
 
-MyBlob.prototype.upload = function (cb) {
-	var that = this;
-	return Bluebird.try(function () {
-		if (that._uploaded) {
-			return that._blobID;
+	getArrayBuffer() {
+		return new Bluebird((resolve) => {
+			var reader = new FileReader();
+
+			if (reader.addEventListener) {
+				reader.addEventListener("loadend", resolve);
+			} else {
+				reader.onloadend = resolve;
+			}
+
+			reader.readAsArrayBuffer(this._blobData);
+		}).then((event) => {
+			var target = event.currentTarget || event.target;
+
+			return target.result;
+		});
+	}
+
+	encryptAndUpload (key, cb) {
+		var blobKey;
+
+		return Bluebird.try(() => {
+			return this.encrypt();
+		}).then((_blobKey) => {
+			blobKey = _blobKey;
+			return keyStore.sym.symEncryptKey(blobKey, key);
+		}).then(() => {
+			return this.upload();
+		}).then(() => {
+			return blobKey;
+		}).nodeify(cb);
+	}
+
+	encrypt (cb) {
+		return Bluebird.resolve().then(() => {
+			if (this._uploaded || !this._decrypted) {
+				throw new Error("trying to encrypt an already encrypted or public blob. add a key decryptor if you want to give users access");
+			}
+
+			return Bluebird.all([
+				keyStore.sym.generateKey(null, "blob key"),
+				this.getArrayBuffer()
+			]);
+		}).spread((_key, buf) => {
+			this._key = _key;
+
+			time("blobencrypt" + (this._blobID || this._preReserved));
+			return keyStore.sym.encryptArrayBuffer(buf, this._key, (progress) => {
+				this._encryptProgress.progress(this.getSize() * progress);
+			});
+		}).then((encryptedData) => {
+			this._encryptProgress.progress(this.getSize());
+			timeEnd("blobencrypt" + (this._blobID || this._preReserved));
+			blobServiceDebug(encryptedData.byteLength);
+			this._decrypted = false;
+
+			this._blobData = new Blob([encryptedData], {type: this._blobData.type});
+
+			return this._key;
+		}).nodeify(cb);
+	}
+
+	decrypt (cb) {
+		if (this._decrypted) {
+			return Bluebird.resolve().nodeify(cb);
 		}
 
-		return that.reserveID();
-	}).then(function (blobid) {
-		return socketService.uploadBlob(that._blobData, blobid, that._uploadProgress);
-	}).then(function () {
-		that._uploaded = true;
+		return Bluebird.try(() => {
+			return this.getArrayBuffer();
+		}).then((encryptedData) => {
+			time("blobdecrypt" + this._blobID);
+			return keyStore.sym.decryptArrayBuffer(encryptedData, this._key, (progress) => {
+				this._decryptProgress.progress(this.getSize() * progress)
+			})
+		}).then((decryptedData) => {
+			this._decryptProgress.progress(this.getSize())
+			timeEnd("blobdecrypt" + this._blobID);
 
-		return that._blobID;
-	}).nodeify(cb);
-};
+			this._decrypted = true;
 
-MyBlob.prototype.getBlobID = function () {
-	return this._blobID;
-};
+			this._blobData = new Blob([decryptedData], {type: this._blobData.type});
+		}).nodeify(cb);
+	}
 
-MyBlob.prototype.reserveID = function (cb) {
-	var that = this;
+	getBase64Representation() {
+		return this.getStringRepresentation().then((blobValue) => {
+			return blobValue.split(",")[1];
+		});
+	}
 
-	return Bluebird.try(function () {
-		var meta = that._meta;
-		meta._key = that._key;
-		meta.one = 1;
+	upload (cb) {
+		return Bluebird.try(() => {
+			if (this._uploaded) {
+				return this._blobID;
+			}
 
-		if (that._preReserved) {
-			return socketService.emit("blob.fullyReserveID", {
-				blobid: that._preReserved,
+			return this.reserveID();
+		}).then((blobid) => {
+			return socketService.uploadBlob(this._blobData, blobid, this._uploadProgress);
+		}).then(() => {
+			this._uploaded = true;
+
+			return this._blobID;
+		}).nodeify(cb);
+	}
+
+	getBlobID() {
+		return this._blobID;
+	}
+
+	reserveID (cb) {
+		return Bluebird.try(() => {
+			var meta = this._meta;
+			meta._key = this._key;
+			meta.one = 1;
+
+			if (this._preReserved) {
+				return socketService.emit("blob.fullyReserveID", {
+					blobid: this._preReserved,
+					meta: meta
+				});
+			}
+
+			return socketService.emit("blob.reserveBlobID", {
 				meta: meta
 			});
-		}
+		}).then((data) => {
+			if (data.blobid) {
+				this._blobID = data.blobid;
 
-		return socketService.emit("blob.reserveBlobID", {
-			meta: meta
+				knownBlobs[this._blobID] = Bluebird.resolve(this);
+
+				return this._blobID;
+			}
+		}).nodeify(cb);
+	}
+
+	preReserveID (cb) {
+		return Bluebird.try(() => {
+			return socketService.emit("blob.preReserveID", {});
+		}).then((data) => {
+			if (data.blobid) {
+				this._preReserved = data.blobid;
+				knownBlobs[this._preReserved] = Bluebird.resolve(this);
+				return data.blobid;
+			}
+
+			throw new Error("got no blobid");
+		}).nodeify(cb);
+	}
+
+	toURL() {
+		var blobToDataURI = Bluebird.promisify(h.blobToDataURI.bind(h));
+
+		return Bluebird.try(() => {
+			if (this._legacy) {
+				return this._blobData;
+			}
+
+			if (this._blobData.localURL) {
+				return this._blobData.localURL;
+			}
+
+			if (typeof window.URL !== "undefined") {
+				return window.URL.createObjectURL(this._blobData);
+			}
+
+			if (typeof webkitURL !== "undefined") {
+				return window.webkitURL.createObjectURL(this._blobData);
+			}
+
+			return blobToDataURI(this._blobData);
+		}).catch(() => {
+			return "";
+		})
+	}
+
+	getStringRepresentation() {
+		var blobToDataURI = Bluebird.promisify(h.blobToDataURI.bind(h));
+
+		return Bluebird.try(() => {
+			if (this.legacy) {
+				return this._blobData;
+			}
+
+			return blobToDataURI(this._blobData);
 		});
-	}).then(function (data) {
-		if (data.blobid) {
-			that._blobID = data.blobid;
+	}
 
-			knownBlobs[that._blobID] = Bluebird.resolve(that);
+	getHash() {
+		return this.getArrayBuffer().then((buf) => {
+			return keyStore.hash.hashArrayBuffer(buf)
+		})
+	}
+}
 
-			return that._blobID;
-		}
-	}).nodeify(cb);
-};
-
-MyBlob.prototype.preReserveID = function (cb) {
-	var that = this;
-	return Bluebird.try(function () {
-		return socketService.emit("blob.preReserveID", {});
-	}).then(function (data) {
-		if (data.blobid) {
-			that._preReserved = data.blobid;
-			knownBlobs[that._preReserved] = Bluebird.resolve(that);
-			return data.blobid;
-		}
-
-		throw new Error("got no blobid");
-	}).nodeify(cb);
-};
-
-MyBlob.prototype.toURL = function (cb) {
-	var blobToDataURI = Bluebird.promisify(h.blobToDataURI.bind(h));
-
-	var that = this;
-	return Bluebird.try(function () {
-		if (that._legacy) {
-			return that._blobData;
-		}
-
-		if (that._blobData.localURL) {
-			return that._blobData.localURL;
-		}
-
-		if (typeof window.URL !== "undefined") {
-			return window.URL.createObjectURL(that._blobData);
-		}
-
-		if (typeof webkitURL !== "undefined") {
-			return window.webkitURL.createObjectURL(that._blobData);
-		}
-
-		return blobToDataURI(that._blobData);
-	}).catch(function (e) {
-		console.error(e);
-
-		return "";
-	}).nodeify(cb);
-};
-
-MyBlob.prototype.getStringRepresentation = function () {
-	var blobToDataURI = Bluebird.promisify(h.blobToDataURI.bind(h));
-
-	return Bluebird.resolve().bind(this).then(function () {
-		if (this.legacy) {
-			return this._blobData;
-		}
-
-		return blobToDataURI(this._blobData);
-	});
-};
-
-MyBlob.prototype.getHash = function () {
-	return this.getArrayBuffer().then(function (buf) {
-		return keyStore.hash.hashArrayBuffer(buf)
-	})
-};
-
-function loadBlobFromServer(blobID, downloadProgress) {
-	return downloadBlobQueue.enqueue(1, function () {
-		return initService.awaitLoading().then(function () {
+const loadBlobFromServer = (blobID, downloadProgress) => {
+	return downloadBlobQueue.enqueue(1, () => {
+		return initService.awaitLoading().then(() => {
 			return new BlobDownloader(socketService, blobID, downloadProgress).download()
-		}).then(function (data) {
+		}).then((data) => {
 			var blob = new MyBlob(data.blob, blobID, { meta: data.meta });
 
 			blobCache.store(blob.getBlobID(), blob._meta, blob._blobData);
@@ -300,8 +293,8 @@ function loadBlobFromServer(blobID, downloadProgress) {
 	});
 }
 
-function loadBlobFromDB(blobID) {
-	return blobCache.get(blobID).then(function (data) {
+const loadBlobFromDB = (blobID) => {
+	return blobCache.get(blobID).then((data) => {
 		if (typeof data.blob === "undefined" || data.blob === false) {
 			throw new Error("cache invalid!");
 		}
@@ -316,23 +309,23 @@ function loadBlobFromDB(blobID) {
 	});
 }
 
-function loadBlob(blobID, downloadProgress) {
-	return loadBlobFromDB(blobID).catch(function () {
+const loadBlob = (blobID, downloadProgress) => {
+	return loadBlobFromDB(blobID).catch(() => {
 		return loadBlobFromServer(blobID, downloadProgress);
 	});
 }
 
 var blobService = {
-	createBlob: function (blob) {
+	createBlob: (blob) => {
 		return new MyBlob(blob);
 	},
-	getBlob: function (blobID, downloadProgress) {
+	getBlob: (blobID, downloadProgress) => {
 		if (!knownBlobs[blobID]) {
 			knownBlobs[blobID] = loadBlob(blobID, downloadProgress)
 		}
 
 		return knownBlobs[blobID]
 	}
-};
+}
 
 module.exports = blobService;
