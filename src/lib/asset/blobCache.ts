a1a0051a4c4c78e8c0389600f03e4f48954b50d8
Martin Czuchra <martin.czuchra@googlemail.com>
2017-08-07 15:08:04 +0200

    Don't save meta alongside blobs.

--- src/lib/asset/blobCache.ts
+++ src/lib/asset/blobCache.ts
@@ -19,12 +19,8 @@ cache.deleteAll()
 const file = new File()
 
 const readFileAsBlob = (path, fileName, type) => file.readAsArrayBuffer(path, fileName).then((buf) => new Blob([buf], { type }))
-const readFileAsJSON = (path, fileName) => file.readAsText(path, fileName).then((text) => JSON.parse(text))
 
-const writeBlobToFile = (path, fileName, data: Blob) => file.writeFile(path, fileName, data)
-const writeJSONToFile = (path, fileName, data) => file.writeFile(path, fileName, JSON.stringify(data))
-
-const generateInfoFileName = (blobID) => `cache-blob-info-${blobID}`
+const writeToFile = (path, fileName, data: Blob) => file.writeFile(path, fileName, data)
 
 const existsFile = (path, fileName) =>
 	file.checkFile(path, fileName).catch((e) => {
@@ -35,24 +31,10 @@ const existsFile = (path, fileName) =>
 		return Bluebird.reject(e)
 	})
 
-const blobInfoMatches = (info, storedBlobInfo) => {
-	return h.deepEqual(info.meta, storedBlobInfo.meta)
-}
-
-const writeCacheInfoFile = (blobID, info) =>
-	writeJSONToFile(file.cacheDirectory, generateInfoFileName(blobID), info)
-
-const readCacheInfoFile = (blobID) =>
-	readFileAsJSON(file.cacheDirectory, generateInfoFileName(blobID))
-
-const removeCacheInfoFile = (blobID) =>
-	file.removeFile(file.cacheDirectory, generateInfoFileName(blobID))
-
-const existsCacheInfoFile = (blobID) =>
-	existsFile(file.cacheDirectory, generateInfoFileName(blobID))
-
 const blobCache = {
+
 	store: (blob) => {
+		// can throw
 		return Bluebird.try(async () => {
 			const blobID = blob.getBlobID()
 
@@ -60,100 +42,52 @@ const blobCache = {
 				throw new Error("trying to store an undecrypted blob")
 			}
 
-			const info = {
-				meta: blob.getMeta(),
-				path: file.cacheDirectory,
-				file: `cache-blob-file-${blobID}`
+			const path = file.cacheDirectory
+			const filename = `${blobID}.blob`
+
+			const exists = await existsFile(path, filename)
+
+			if (!exists) {
+				console.info('saving blob...')
+				await writeToFile(path, filename, blob.getBlobData())
+				console.info('blob saved')
 			}
 
-			const exists = await existsCacheInfoFile(blobID)
+			console.info(`blob is in ${filename}`)
+			return `${path}${filename}`
 
-			if (exists) {
-				const storedBlobInfo = await readCacheInfoFile(blobID)
-				const storedBlobExists = await existsFile(storedBlobInfo.path, storedBlobInfo.file)
-
-				if (storedBlobExists && blobInfoMatches(info, storedBlobInfo)) {
-					return `${storedBlobInfo.path}${storedBlobInfo.file}`
-				}
-
-				await removeCacheInfoFile(blobID)
-				await file.removeFile(storedBlobInfo.path, storedBlobInfo.file)
-
-				await writeBlobToFile(storedBlobInfo.path, storedBlobInfo.file, blob.getBlobData())
-
-				info.file = storedBlobInfo.file
-				info.path = storedBlobInfo.path
-			} else {
-				await writeBlobToFile(info.path, info.file, blob.getBlobData())
-			}
-
-			await writeCacheInfoFile(blobID, info)
-
-			return `${info.path}${info.file}`
 		})
 	},
+
 	getBlobUrl: (blobID) => {
+		// can throw
 		return Bluebird.try(async () => {
-			const exists = await existsCacheInfoFile(blobID)
+			const path = file.cacheDirectory
+			const filename = `${blobID}.blob`
+			const exists = await existsFile(path, filename)
 
 			if (!exists) {
-				throw new Error("blob not cached")
+				console.warn(`cannot get blob url, blob does not exist: ${filename}`)
+				throw new Error(`cannot get blob url, blob does not exist: ${filename}`)
 			}
 
-			const { path, file } = await readCacheInfoFile(blobID)
-
-			const blobFileExists = await existsFile(path, file)
-
-			if (!blobFileExists) {
-				await removeCacheInfoFile(blobID)
-
-				throw new Error("blob not cached")
-			}
-
-			return `${path}${file}`
+			return `${path}${filename}`
 		})
 	},
-	isLoaded: (blobID) => {
-		return Bluebird.try(async () => {
-			const exists = await existsCacheInfoFile(blobID)
 
-			if (!exists) {
-				return false
-			}
+	isLoaded: (blobID) => blobCache.getBlobUrl(blobID).then(() => true).catch(() => false),
 
-			const { path, file } = await readCacheInfoFile(blobID)
-
-			const blobFileExists = await existsFile(path, file)
-
-			if (!blobFileExists) {
-				await removeCacheInfoFile(blobID)
-
-				return false
-			}
-
-			return true
-		})
-	},
 	get: (blobID) => {
-		const fileName = generateInfoFileName(blobID)
 
 		return Bluebird.try(async () => {
-			const exists = await existsCacheInfoFile(blobID)
+			const path = file.cacheDirectory
+			const filename = `${blobID}.blob`
 
-			if (!exists) {
-				throw new Error(`blob cache miss for ${blobID}`);
-			}
+			console.warn(`reading blob from file ${filename}...`)
+			const blob = await readFileAsBlob(path, filename, "")
+			console.warn(`successfully read blob from file ${filename}...`)
 
-			const fileInfo = await readCacheInfoFile(blobID)
-
-			const blob = await readFileAsBlob(fileInfo.path, fileInfo.file, "")
-
-			return {
-				blob,
-				blobID,
-				decrypted: fileInfo.decrypted,
-				meta: fileInfo.meta
-			}
+			return { blob, blobID, decrypted: true, meta: {} }
 		})
 	}
 }
