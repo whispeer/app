ab3425b931c9d16132bfdf1f50ca0e0d800fec86
Martin Czuchra <martin.czuchra@googlemail.com>
2017-08-08 16:23:03 +0200

    Minor refactors of blobCache.

--- src/lib/asset/blobCache.ts
+++ src/lib/asset/blobCache.ts
@@ -1,33 +1,20 @@
 import * as Bluebird from "bluebird"
-import { File } from '@ionic-native/file';
+import { File } from '@ionic-native/file'
 
 import Cache from "../services/Cache"
 import h from "../helper/helper"
 
-const cache = new Cache("blobs");
-
-const uriToBlob = (blob) => {
-	if (typeof blob === "string") {
-		return h.dataURItoBlob(blob);
-	}
-
-	return blob
-}
-
-cache.deleteAll()
+const uriToBlob = (blob) =>
+	typeof blob === "string" ? h.dataURItoBlob(blob) : blob
 
 const file = new File()
-
 const readFileAsBlob = (path, fileName, type) => file.readAsArrayBuffer(path, fileName).then((buf) => new Blob([buf], { type }))
-
 const writeToFile = (path, fileName, data: Blob) => file.writeFile(path, fileName, data)
-
 const existsFile = (path, fileName) =>
 	file.checkFile(path, fileName).catch((e) => {
 		if (e.code === 1) {
 			return false
 		}
-
 		return Bluebird.reject(e)
 	})
 
@@ -39,7 +26,6 @@ const blobCache = {
 	},
 
 	store: (blob) => {
-		// can throw
 		return Bluebird.try(async () => {
 			const blobID = blob.getBlobID()
 
@@ -49,30 +35,24 @@ const blobCache = {
 
 			const path = file.cacheDirectory
 			const filename = idToFileName(blobID)
-
 			const exists = await existsFile(path, filename)
 
 			if (!exists) {
-				console.info('saving blob...')
 				await writeToFile(path, filename, blob.getBlobData())
-				console.info('blob saved')
 			}
 
-			console.info(`blob is in ${filename}`)
 			return `${path}${filename}`
 
 		})
 	},
 
 	getBlobUrl: (blobID) => {
-		// can throw
 		return Bluebird.try(async () => {
 			const path = file.cacheDirectory
 			const filename = idToFileName(blobID)
 			const exists = await existsFile(path, filename)
 
 			if (!exists) {
-				console.warn(`cannot get blob url, blob does not exist: ${filename}`)
 				throw new Error(`cannot get blob url, blob does not exist: ${filename}`)
 			}
 
@@ -87,14 +67,14 @@ const blobCache = {
 		return Bluebird.try(async () => {
 			const path = file.cacheDirectory
 			const filename = idToFileName(blobID)
-
-			console.warn(`reading blob from file ${filename}...`)
 			const blob = await readFileAsBlob(path, filename, "")
-			console.warn(`successfully read blob from file ${filename}...`)
-
 			return { blob, blobID, decrypted: true, meta: {} }
 		})
 	}
 }
 
 export default blobCache
+
+// delete previous cache
+;(new Cache("blobs")).deleteAll()
+
