b100b771f2b94e686fa848e2b6a4b44efb0cb8ba
Martin Czuchra <martin.czuchra@googlemail.com>
2017-08-08 16:36:57 +0200

    Async cache directory getting.

--- src/lib/asset/blobCache.ts
+++ src/lib/asset/blobCache.ts
@@ -4,13 +4,15 @@ import { File } from '@ionic-native/file'
 import Cache from "../services/Cache"
 import h from "../helper/helper"
 
-const file = new File()
-const readFileAsBlob = (path, fileName, type) =>
-	file.readAsArrayBuffer(path, fileName).then((buf) => new Blob([buf], { type }))
-const writeToFile = (path, fileName, data: Blob) =>
-	file.writeFile(path, fileName, data)
-const existsFile = (path, fileName) =>
-	file.checkFile(path, fileName).catch((e) => {
+const BLOB_CACHE_DIR = "blobCache"
+const FILE = new File()
+
+const readFileAsBlob = (path, filename, type) =>
+	FILE.readAsArrayBuffer(path, filename).then((buf) => new Blob([buf], { type }))
+const writeToFile = (path, filename, data: Blob) =>
+	FILE.writeFile(path, filename, data)
+const existsFile = (path, filename) =>
+	FILE.checkFile(path, filename).catch((e) => {
 		if (e.code === 1) {
 			return false
 		}
@@ -18,10 +20,22 @@ const existsFile = (path, fileName) =>
 	})
 
 const idToFileName = (blobID) => `${blobID}.blob`
+const getCacheDirectory = () => {
+	return Bluebird.resolve(FILE.cacheDirectory)
+}
 
 const blobCache = {
-	moveFileToBlob: (fileDirectory, fileName, blobID) => {
-		return file.moveFile(fileDirectory, fileName, file.cacheDirectory, idToFileName(blobID))
+
+	clear: () => {
+		console.error('Clearing the cache is not implemented')
+	},
+
+	moveFileToBlob: (currentDirectory, currentFilename, blobID) => {
+		return Bluebird.try(async () => {
+			const path = await getCacheDirectory()
+			const filename = idToFileName(blobID)
+			return FILE.moveFile(currentDirectory, currentFilename, path, filename)
+		})
 	},
 
 	store: (blob) => {
@@ -32,7 +46,7 @@ const blobCache = {
 				throw new Error("trying to store an undecrypted blob")
 			}
 
-			const path = file.cacheDirectory
+			const path = await getCacheDirectory()
 			const filename = idToFileName(blobID)
 			const exists = await existsFile(path, filename)
 
@@ -47,7 +61,7 @@ const blobCache = {
 
 	getBlobUrl: (blobID) => {
 		return Bluebird.try(async () => {
-			const path = file.cacheDirectory
+			const path = await getCacheDirectory()
 			const filename = idToFileName(blobID)
 			const exists = await existsFile(path, filename)
 
@@ -64,7 +78,7 @@ const blobCache = {
 	get: (blobID) => {
 
 		return Bluebird.try(async () => {
-			const path = file.cacheDirectory
+			const path = await getCacheDirectory()
 			const filename = idToFileName(blobID)
 			const blob = await readFileAsBlob(path, filename, "")
 			return { blob, blobID, decrypted: true, meta: {} }
