9251ea4f83f0141a616dc0b284f9a83cd19b8926
Martin Czuchra <martin.czuchra@googlemail.com>
2017-09-19 19:50:01 +0200

    Fix position:fixed update delay.

--- src/components/mainMenu.ts
+++ src/components/mainMenu.ts
@@ -1,5 +1,5 @@
 import { Component, Input, Output, EventEmitter } from "@angular/core"
-import { NavController, FabContainer } from "ionic-angular"
+import { NavController } from "ionic-angular"
 
 import sessionService from "../lib/services/session.service"
 
