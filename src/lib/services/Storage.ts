179b4b39beb309433430b74e7dd8cf495acf6b2c
Nils Kenneweg <nk@ovt.me>
2017-10-04 16:00:02 +0200

    fix storage

--- src/lib/services/Storage.ts
+++ src/lib/services/Storage.ts
@@ -1,29 +1,37 @@
-import * as Bluebird from "bluebird";
+import * as Bluebird from "bluebird"
 
 export default class Storage {
 	private _prefix: string
 
 	constructor(prefix: string) {
-		this._prefix = prefix;
+		this._prefix = prefix
 	}
 
 	get = (key: string): any =>
 		localStorage.getItem(this.calculateKey(key))
 
 	set = (key: string, data: any) =>
-		localStorage.setItem(this.calculateKey(key), data);
+		localStorage.setItem(this.calculateKey(key), data)
 
 	remove = (key: string) =>
-		localStorage.removeItem(this.calculateKey(key));
+		localStorage.removeItem(this.calculateKey(key))
 
-	clear = () =>
-		Bluebird.resolve(localStorage.clear())
+	clear = () => {
+		const usedKeys = Object.keys(localStorage)
+		usedKeys.filter(function (key) {
+			return key.indexOf(this._prefix) === 0
+		}, this).forEach(function (key) {
+			localStorage.removeItem(key)
+		})
+
+		return Bluebird.resolve()
+	}
 
 	save = () =>
 		Bluebird.resolve()
 
 	private calculateKey = (key: string) =>
-		this._prefix + "." + key;
+		this._prefix + "." + key
 
 	awaitLoading = () => Bluebird.resolve()
 }
