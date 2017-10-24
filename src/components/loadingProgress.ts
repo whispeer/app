e320b16aa02beab7730567fc64b9dd7f99f4788c
Martin Czuchra <martin.czuchra@googlemail.com>
2017-10-24 12:50:04 +0200

    Punch a hole in the progress bar.

--- src/components/loadingProgress.ts
+++ src/components/loadingProgress.ts
@@ -7,36 +7,27 @@ import { Component, Input, ElementRef } from "@angular/core";
 export class LoadingProgress {
 
 	className: string = ""
-	background: string
 
 	@Input() progress: any
-	@Input() inverse: boolean = false
 	@Input() parentBackgroundClass: string
 
-	@Input()
-	set color(color: string) {
-		color = color || ""
-		this.className = "radial-progress"
-		if(color.length > 0) {
-			this.className = `${this.className}--${color}`
-		}
-		console.log(this.className)
-	}
-
-	private fetchBackground() {
+	private getBackgroundElement() {
 		let element = this.element.nativeElement.parentElement
 		while (element && !element.classList.contains(this.parentBackgroundClass)) {
 			element = element.parentElement
 		}
-		if (!element) { return 'inherit' }
-		return window.getComputedStyle(element)
-			.getPropertyValue('background-color')
+		return element
 	}
 
-	getProgress = () => {
-		if (!this.background) { this.background = this.fetchBackground() }
-		return Math.floor(this.progress() * 100)
-
+	private _background:string;
+	get background():string {
+		if (!this._background !== undefined) {
+			const element = this.getBackgroundElement()
+			if (!element) { this._background = 'inherit' }
+			this._background = window.getComputedStyle(element)
+			.getPropertyValue('background-color')
+		}
+		return this._background
 	}
 
 	constructor(private element: ElementRef) { }
