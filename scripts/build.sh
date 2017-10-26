440c267cb64911d1c97bbd73fa0c9bef8b80df16
Nils Kenneweg <nk@ovt.me>
2017-10-26 11:43:18 +0200

    Revert "android 4 needs no special treatment any more"
    This reverts commit 024725b3cfa0dab1b4f449c14389dc8318e487c0.

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,15 +14,34 @@ function signAndCopy {
 
 npm run businessSass
 
-./scripts/setVersionCode.js 0
+echo "build for android >= 5"
+
+sed -i .bak 's/android-minSdkVersion" value="16"/android-minSdkVersion" value="21"/g' config.xml
+
+./scripts/setVersionCode.js 5
 
 rm -rf platforms plugins www/*
 cordova prepare
+
+# sed -i .bak 's/^ext.cdvMinSdkVersion/\/\/ext.cdvMinSdkVersion/g' ./plugins/phonegap-plugin-barcodescanner/src/android/barcodescanner.gradle
 
 ionic cordova build android --prod --release
 cp ./platforms/android/build/outputs/apk/android-release-unsigned.apk ./release/whispeer-android-unsigned.apk
 
 mv config.xml.bak config.xml
 
+echo "build for android 4"
+
+cordova plugin add cordova-plugin-crosswalk-webview
+./scripts/setVersionCode.js 0
+
+ionic cordova build --release android
+cp ./platforms/android/build/outputs/apk/android-release-unsigned.apk ./release/whispeer-android4-unsigned.apk
+
+rm -rf platforms plugins www/*
+
 extra=android
+signAndCopy
+
+extra=android4
 signAndCopy
--- scripts/build_ios.sh
+++ scripts/build_ios.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
-set -e
-set -x
-
 npm run businessSass
 rm -rf node_modules platforms plugins www/*
 yarn
