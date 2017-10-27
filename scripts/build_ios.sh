5e335416960333792282cedd4df4b302a3490634
Nils Kenneweg <nk@ovt.me>
2017-10-27 17:29:55 +0200

    update build script

--- scripts/build_ios.sh
+++ scripts/build_ios.sh
@@ -2,9 +2,8 @@
 set -e
 set -x
 
-npm run businessSass
 rm -rf node_modules platforms plugins www/*
 yarn
-cordova prepare
+npm run businessSass
 ionic build --prod
 open ./platforms/ios/whispeer\ Messenger.xcworkspace
