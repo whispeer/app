ea7dd2ccc87b5fa0d5c6f585b176565845ebc279
Nils Kenneweg <nk@ovt.me>
2017-10-27 16:02:23 +0200

    new android/ios commits. faster build

--- scripts/build_ios.sh
+++ scripts/build_ios.sh
@@ -6,5 +6,5 @@ npm run businessSass
 rm -rf node_modules platforms plugins www/*
 yarn
 cordova prepare
-ionic cordova build ios --prod
+ionic build --prod
 open ./platforms/ios/whispeer\ Messenger.xcworkspace
