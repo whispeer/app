76011ec8890228ef9f636cc69b6f0e57575eab7d
Nils Kenneweg <nk@ovt.me>
2017-11-14 15:01:31 +0100

    correct url for backup

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -91,7 +91,7 @@ export class SettingsPage {
 			ctx.fillText(keyData.substr(26), 10, image.height + 75);
 
 			ctx.fillText("whispeer-Passwort vergessen?", 10, image.height + 125);
-			ctx.fillText("https://whilogispeer.de/recovery", 10, image.height + 150);
+			ctx.fillText("https://whispeer.de/recovery", 10, image.height + 150);
 
 			this.photoViewer.show(c.toDataURL())
 		})
