609b6d8aef32e13aa5c00a0e9aad80e840e25670
Nils Kenneweg <nk@ovt.me>
2017-11-16 13:22:07 +0100

    bugfix

--- src/lib/angularUtils.ts
+++ src/lib/angularUtils.ts
@@ -2,6 +2,6 @@ import { NavController, NavOptions } from "ionic-angular";
 
 export const replaceView = (navCtrl: NavController, view: string, params?, count = 1, options?: NavOptions) => {
 	navCtrl.push(view, params, options).then(() => {
-		navCtrl.remove(this.navCtrl.length() - count - 1, count)
+		navCtrl.remove(navCtrl.length() - count - 1, count)
 	})
 }
