450110a5a6a988b896d44563145001cd1d39634d
Nils Kenneweg <nk@ovt.me>
2018-01-31 15:06:16 +0100

    do not resort blocked chats

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -152,21 +152,7 @@ export class HomePage {
 			.filter((chatID) => ChatLoader.isLoaded(chatID))
 			.filter((id, i) => i < this.numberOfChatsToDisplay)
 			.map((chatID) => getChatMemoizer(chatID).getValue())
-			.sort((a, b) => {
-				if(a.allBlocked && b.allBlocked) {
-					return 0
-				}
-
-				if(a.allBlocked) {
-					return 1
-				}
-
-				if(b.allBlocked) {
-					return -1
-				}
-
-				return b.time - a.time
-			})
+			.sort((a, b) => b.time - a.time)
 	}
 
 	showNoConversationsPlaceholder = () => !this.chatsLoading && this.getChatCount() === 0
